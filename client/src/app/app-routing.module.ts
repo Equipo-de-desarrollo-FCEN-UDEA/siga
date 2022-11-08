60bda84f02920702ea07f7b34847698089f241c0
Simon3640 <simon.garcial@udea.edu.co>
2022-11-08 18:09:22 -0500

    Tabla usuarios terminada, falta rol

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -6,7 +6,12 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
-  }
+  },
+  { 
+    path: 'usuarios', 
+    loadChildren: () => import('./modules/users/users.module')
+    .then(m => m.UsersModule) 
+  },
 ];
 
 @NgModule({
