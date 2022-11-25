27e21d6223377d860e00b1cab7b67b9cc4c83f75
simon <simon.garcial@udea.edu.co>
2022-11-25 15:44:16 -0500

    generar documento resolucion

--- backend/app/services/documents/applications/commission.py
+++ backend/app/services/documents/applications/commission.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 from jinja2 import Environment, FileSystemLoader
+from weasyprint import HTML
 
 from app.domain.models import Commission, User
 from app.core.logging import get_logging
@@ -9,6 +10,11 @@ from ..templates import templates_dir
 log = get_logging(__name__)
 
 def resolution_generation(user: User, commission: Commission):
-    log.debug(user.__dict__)
-    log.debug(commission.__dict__)
-    log.debug(templates_dir)
+    env = Environment(loader=FileSystemLoader(templates_dir))
+    data = {
+        "image": "logo_2.png",
+        "signature": "signature_2.png",
+        "date": datetime.now().strftime('%Y/%m/%d'),
+        "user": user.__dict__,
+        "school": user.department.school.__dict__
+    }
