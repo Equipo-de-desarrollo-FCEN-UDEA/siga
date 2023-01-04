d7b0150ba213f46d4a9da5f33668a1d7b3678c18
DavidTorres-sys <david.torresg@udea.edu.co>
2023-01-04 11:56:59 -0500

    avance plan de trabajo

--- client/src/app/modules/applications/pages/create/components/full-time/pages/workplan/workplan.component.ts
+++ client/src/app/modules/applications/pages/create/components/full-time/pages/workplan/workplan.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Validators, FormBuilder, FormArray } from '@angular/forms';
 
 @Component({
@@ -12,7 +12,9 @@ export class WorkplanComponent implements OnInit {
     semester: ['', [Validators.required, Validators.maxLength(10), Validators.minLength(6)]],
     register: ['', [Validators.required]],
     part_time: [NaN, [Validators.required]],
-    teaching_activities: this.fb.array([this.TeachingActivitiesGroup()], [Validators.required]),
+    teaching_activities: this.fb.array([this.teachingActivitiesGroup()], [Validators.required]),
+    research_activities: this.fb.array([this.researchActivitiesGroup()], [Validators.required]),
+    extension_activities: this.fb.array([this.extensionActivitiesGroup()], [Validators.required]),
   });
 
   // ------------------------------
@@ -23,8 +25,13 @@ export class WorkplanComponent implements OnInit {
     return this.f_workplan.get('teaching_activities') as FormArray;
   }
 
-  @ViewChild('c1')
-  private c1!: ElementRef;
+  get researchActivitiesArr(): FormArray {
+    return this.f_workplan.get('research_activities') as FormArray;
+  }
+
+  get extensionActivitiesArr(): FormArray {
+    return this.f_workplan.get('extension_activities') as FormArray;
+  }
 
   constructor(
     private fb: FormBuilder,
@@ -42,7 +49,7 @@ export class WorkplanComponent implements OnInit {
   // --------- GROUPS -----------
   // ------------------------------
 
-  TeachingActivitiesGroup() {
+  teachingActivitiesGroup() {
     return this.fb.group({
       activity_identification: this.fb.group({
         code: ['', [Validators.required, Validators.maxLength(255), Validators.minLength(6)]],
@@ -60,7 +67,29 @@ export class WorkplanComponent implements OnInit {
         weekly: [NaN, [Validators.required]],
         biannual: [NaN, [Validators.required]],
       })
-    })
+    });
+  }
+
+  researchActivitiesGroup() {
+    return this.fb.group({
+      code: ['', [Validators.required, Validators.maxLength(255), Validators.minLength(6)]],
+      project_identification: ['', [Validators.required, Validators.maxLength(255), Validators.minLength(6)]],
+      responsibility: ['', [Validators.required, Validators.maxLength(255), Validators.minLength(6)]],
+      cost: ['', [Validators.required, Validators.maxLength(255), Validators.minLength(6)]],
+      support_certificate: ['', [Validators.required, Validators.maxLength(255), Validators.minLength(6)]],
+      semester_hours: [NaN, [Validators.required]],
+    });
+  }
+
+  extensionActivitiesGroup() {
+    return this.fb.group({
+      code: ['', [Validators.required, Validators.maxLength(255), Validators.minLength(6)]],
+      activity_identification: ['', [Validators.required, Validators.maxLength(255), Validators.minLength(6)]],
+      responsibility: ['', [Validators.required, Validators.maxLength(255), Validators.minLength(6)]],
+      cost:  ['', [Validators.required, Validators.maxLength(255), Validators.minLength(6)]],
+      hours_week: [NaN, [Validators.required]],
+      semester_hours: [NaN, [Validators.required]],
+    });
   }
 
   // ------------------------------
@@ -72,10 +101,31 @@ export class WorkplanComponent implements OnInit {
     if (div) {
       let height = div.clientHeight;
       div.style.maxHeight = height + 'px';
-      this.teachingActivitiesArr.push(this.TeachingActivitiesGroup());
+      this.teachingActivitiesArr.push(this.teachingActivitiesGroup());
       setTimeout(() => div!.scrollTop = div!.scrollHeight, 100);
     }
   }
+
+  addResearchActivities() {
+    let div = document.getElementById('c8')
+    if (div) {
+      let height = div.clientHeight;
+      div.style.maxHeight = height + 'px';
+      this.researchActivitiesArr.push(this.researchActivitiesGroup());
+      setTimeout(() => div!.scrollTop = div!.scrollHeight, 100);
+    }
+  }
+
+  addExtensionActivities() {
+    let div = document.getElementById('c2')
+    if (div) {
+      let height = div.clientHeight;
+      div.style.maxHeight = height + 'px';
+      this.extensionActivitiesArr.push(this.extensionActivitiesGroup());
+      setTimeout(() => div!.scrollTop = div!.scrollHeight, 100);
+    }
+  }
+
 
   // ------------------------------
   // --------- PATCHS -----------
@@ -86,7 +136,20 @@ export class WorkplanComponent implements OnInit {
       this.addTeachingActivities();
     }
     this.teachingActivitiesArr.patchValue(activity);
+  }
 
+  patchResearchActivities(activity: any) {
+    for (let i = 0; i < activity.length -1; i++) {
+      this.addResearchActivities();
+    }
+    this.researchActivitiesArr.patchValue(activity);
+  }
+
+  patchActividadesExtension(activity: any) {
+    for (let i = 0; i < activity.length -1 ; i++) {
+      this.addExtensionActivities();
+    }
+    this.extensionActivitiesArr.patchValue(activity);
   }
 
   // ------------------------------
