9d407f606cc1db0e628b9fd165029ac5449c5b22
alwos19 <jhon.jaramilloe@udea.edu.co>
2023-03-05 13:10:29 -0500

    formulario crear vacaciones casi listo

--- client/src/app/modules/applications/pages/create/components/vacation/vacation.component.ts
+++ client/src/app/modules/applications/pages/create/components/vacation/vacation.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectorRef, Component, NgZone, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { DocumentsResponse, file_path } from '@interfaces/documents';
 import { NgbCalendar, NgbDate, NgbDateParserFormatter, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 import { ApplicationTypesService } from '@services/application-types.service';
@@ -11,6 +11,9 @@ import { DocumentService } from '@services/document.service';
 import { switchMap } from 'rxjs';
 import { ApplicationSubTypeService } from '@services/application-sub-type.service';
 import { SignaturePad } from 'angular2-signaturepad';
+import { LaboralDays } from '@shared/utils';
+import { Holiday } from '@interfaces/holiday';
+import { HolidayService } from '@services/holiday.service';
 
 
 @Component({
@@ -28,26 +31,29 @@ export class VacationComponent implements OnInit {
   public today = this.calendar.getToday();
   public laboralDay: number = 0;
 
-  public files : any[] = [];
+  public files: any[] = [];
   public archivos = [1];
   public documents: file_path[] = []
 
-   // Signature
-   @ViewChild(SignaturePad) signaturePad!: SignaturePad;
-   signatureImg: string="";
- 
-   signaturePadOptions: Object = { 
-     'minWidth': 2,
-     'canvasWidth': 1000,
-     'canvasHeight': 200
-   };
+  // Signature
+  @ViewChild(SignaturePad) signaturePad!: SignaturePad;
+  signatureImg: string = "";
+
+  signaturePadOptions: Object = {
+    'minWidth': 2,
+    'canvasWidth': 1000,
+    'canvasHeight': 200
+  };
 
   // For handle errors
   public clicked = 0;
   public error = '';
   public submitted = false;
 
+  public id: number = 0;
+
   public applicationType$ = this.applicationTypeSvc.getApplicationType(5);
+
 
   // --------------------------------------------------
   // ----------- MANEJO DE ERRORES EN EL FORM ---------
@@ -55,36 +61,44 @@ export class VacationComponent implements OnInit {
 
   get f() { return this.form.controls; }
 
+  // holidays
+  public holidays: Holiday[] = [];
   constructor(
 
     private fb: FormBuilder,
-    private calendar : NgbCalendar,
+    private calendar: NgbCalendar,
     public formatter: NgbDateParserFormatter,
     private ngZone: NgZone,
     private router: Router,
+    private route: ActivatedRoute,
     private cd: ChangeDetectorRef,
 
     private applicationTypeSvc: ApplicationTypesService,
     private SubTypeSvc: ApplicationSubTypeService,
     private vacationSvc: VacationService,
-    private documentSvc: DocumentService
-  
+    private documentSvc: DocumentService,
+    private holidaySvc: HolidayService
+
   ) { }
-  
-// Form vacation
+
+  // Form vacation
   public form = this.fb.group({
     application_sub_type_id: [0, [Validators.required, Validators.min(1)]],
-    total_days: [1,[Validators.required]],
+    total_days: [0, [Validators.required]],
     start_date: [new Date(), [Validators.required]],
     end_date: [new Date(), [Validators.required]],
     documents: [this.documents],
-    signature:[this.signatureImg], 
-    })
+    signature: [this.signatureImg],
+  })
 
-  submit(){
+  // --------------------------------------
+  // ------------ SUBMIT FORM  ------------
+  // --------------------------------------
+
+  submit() {
     this.submitted = true;
     // Se detiene aqui si el formulario es invalido
-    if(this.form.invalid){
+    if (this.form.invalid) {
       Swal.fire({
         title: 'Error',
         text: '¡Revise que haya llenado todos los campos que el Formato sugiere!',
@@ -101,7 +115,7 @@ export class VacationComponent implements OnInit {
     if (this.files.length > 0) {
       vacation = this.documentSvc.postDocument(this.files as File[]).pipe(
         switchMap((data: DocumentsResponse) => {
-          if (data){
+          if (data) {
             this.form.patchValue({
               documents: data.files_paths
             })
@@ -112,30 +126,61 @@ export class VacationComponent implements OnInit {
       )
     }
     vacation.subscribe({
-      next:(data) => {
+      next: (data) => {
         Swal.fire({
-            title:'La solicitud se creó correctamente',
-            icon: 'success',
-            confirmButtonText: 'Aceptar'
-          }
-        ).then((result)=>{
-          if (result.isConfirmed){
+          title: 'La solicitud se creó correctamente',
+          icon: 'success',
+          confirmButtonText: 'Aceptar'
+        }
+        ).then((result) => {
+          if (result.isConfirmed) {
             this.router.navigate([`/solicitudes/ver/${data.id}/vaciones`])
           }
         });
-      },error:(err)=>{
-        this.error =err
+      }, error: (err) => {
+        this.error = err
       }
     })
   }
 
   ngOnInit(): void {
-    this.signaturePad.set('minWidth', 5); // set szimek/signature_pad options at runtime
-    this.signaturePad.clear(); // invoke functions from szimek/signature_pad API
 
+    this.route.parent?.params.subscribe((params) => {
+      this.id = params['id'];
+      this.vacationSvc.getVacation(this.id).subscribe((data) => {
+        // console.log(data);
+        this.form.patchValue({
+          ...data.vacation,
+          application_sub_type_id: data.application_sub_type_id,
+        });
+        // console.log(data.vacation);
+        this.documents = data.vacation.documents!;
+
+        this.SubTypeSvc.getApplicationSubType(+data.application_sub_type_id).subscribe({
+          next: (res) => {
+            this.laboralDay = res.extra.days;
+          },
+        });
+      });
+    });
   }
 
-  // Tipo de solicitud
+  ngAfterViewInit(): void {
+    //this.signaturePad.set('minWidth', 3); // set szimek/signature_pad options at runtime
+    this.signaturePad.set('canvasWidth', 500);
+    this.signaturePad.set('border', "5px #e8e8e800")
+    this.signaturePad.set('border-style', "double");
+    this.signaturePad.clear(); // invoke functions from szimek/signature_pad API
+    this.signaturePad.resizeCanvas();
+    this.holidaySvc.getHolidays().subscribe({
+      next: (data) => {
+        this.holidays = data;
+      },
+    });
+  }
+  // --------------------------------------
+  // --------- VACATION TYPES  ---------
+  // --------------------------------------
 
   onApplicationSubType(event: Event) {
     // Obtener el value antes de los ':'  
@@ -149,9 +194,9 @@ export class VacationComponent implements OnInit {
 
   isInvalidForm(controlName: string) {
     return this.form.get(controlName)?.
-    invalid && this.form.get(controlName)?.touched;
+      invalid && this.form.get(controlName)?.touched;
   }
- // --------------------------------------
+  // --------------------------------------
   // ------------- SIGNATURE -------------
   // --------------------------------------
 
@@ -178,7 +223,7 @@ export class VacationComponent implements OnInit {
     this.signaturePad?.clear();
   }
 
-  savePad(event:any) {
+  savePad(event: any) {
     const base64Data = this.signaturePad?.toDataURL();
     this.signatureImg = base64Data;
     console.log(base64Data);
@@ -190,11 +235,13 @@ export class VacationComponent implements OnInit {
       confirmButtonColor: '#3AB795',
     });
     event.target.disabled = true;
-    return; 
+    return;
   }
-  // --------------------------------------
-  // -------- ARCHIVOS - ANEXOS -----------
-  // --------------------------------------
+
+
+
+
+
 
   // --------------------------------------
   // -------- ARCHIVOS - ANEXOS -----------
@@ -217,7 +264,7 @@ export class VacationComponent implements OnInit {
     });
   }
 
-  onUpload(event:Event, index: number) {
+  onUpload(event: Event, index: number) {
     const element = event.target as HTMLInputElement;
     const file = element.files?.item(0);
     if (file) {
@@ -227,7 +274,8 @@ export class VacationComponent implements OnInit {
 
   removeFile(index: number) {
     if (this.archivos.length > 1) {
-    this.archivos.splice(index, 1);};
+      this.archivos.splice(index, 1);
+    };
     this.files.splice(index, 1);
   }
   validSize() {
@@ -237,13 +285,114 @@ export class VacationComponent implements OnInit {
 
   validFileType() {
     const extensionesValidas = ["png", "jpg", "gif", "jpeg", "pdf"];
-    
-    let flag = true; 
+
+    let flag = true;
     this.files.forEach((file) => {
       flag = extensionesValidas.includes(file.name.split(".")[file.name.split(".").length - 1]);
     })
     return flag;
 
+  }
+
+  // --------------------------------------
+  // ------------- DATEPICKER -------------
+  // --------------------------------------
+
+  selectDays(fromDate: NgbDate | null, toDate: NgbDate | null): boolean {
+    if (fromDate || toDate) {
+      return (
+        LaboralDays(
+          new Date(this.formatter.format(fromDate)),
+          new Date(this.formatter.format(toDate)),
+          this.holidays
+        ) > this.laboralDay
+      );
+    } else {
+      return false;
+    }
+  }
+
+  onDateSelection(date: NgbDate) {
+    if (!this.fromDate && !this.toDate) {
+      console.log('!this.fromDate && !this.toDate')
+      this.fromDate = date;
+      this.form.patchValue({
+        start_date: new Date(
+          this.fromDate!.year,
+          this.fromDate!.month - 1,
+          this.fromDate!.day
+        ),
+      });
+    } else if (this.fromDate && !this.toDate && date) {
+      console.log('this.fromDate && !this.toDate && date', this.fromDate, this.toDate, date)
+      this.toDate = date;
+      this.form.patchValue({
+        end_date: new Date(
+          this.toDate.year,
+          this.toDate.month - 1,
+          this.toDate.day
+        ),
+      });
+    } else {
+      console.log('else', this.fromDate, this.toDate)
+      this.toDate = null;
+      this.fromDate = date
+      this.form.patchValue({
+        start_date: new Date(
+          this.fromDate.year,
+          this.fromDate.month - 1,
+          this.fromDate.day
+        ),
+      });
+      this.form.patchValue({
+        end_date: new Date(
+          this.toDate!.year,
+          this.toDate!.month - 1,
+          this.toDate!.day
+        ),
+      });
+    }
+  }
+
+  isHovered(date: NgbDate) {
+    return (
+      this.fromDate &&
+      !this.toDate &&
+      this.hoveredDate &&
+      date.after(this.fromDate) &&
+      date.before(this.hoveredDate)
+    );
+  }
+
+  isHoveredInvalid(date: NgbDate) {
+    return (
+      this.fromDate &&
+      !this.toDate &&
+      this.hoveredDate &&
+      this.selectDays(this.fromDate, this.hoveredDate) &&
+      date.after(this.fromDate) &&
+      date.before(this.hoveredDate)
+    );
+  }
+
+  isInside(date: NgbDate) {
+    return this.toDate && date.after(this.fromDate) && date.before(this.toDate);
+  }
+
+  isRange(date: NgbDate) {
+    return (
+      date.equals(this.fromDate) ||
+      (this.toDate && date.equals(this.toDate)) ||
+      this.isInside(date) ||
+      this.isHovered(date)
+    );
+  }
+
+  validateInput(currentValue: NgbDate | null, input: string): NgbDate | null {
+    const PARSED = this.formatter.parse(input);
+    return PARSED && this.calendar.isValid(NgbDate.from(PARSED))
+      ? NgbDate.from(PARSED)
+      : currentValue;
   }
 
 }
