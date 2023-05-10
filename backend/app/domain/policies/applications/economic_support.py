445aa0d8fbe32777a6b907b060ee2b8a70bb1aab
Omar Tíjaro <otijaro@gmail.com>
2023-05-10 09:14:57 -0500

    Economic support policies: created

--- backend/app/domain/policies/applications/economic_support.py
+++ backend/app/domain/policies/applications/economic_support.py
@@ -2,14 +2,26 @@ from app.domain.policies.base import Base
 
 from app.domain.models.applications.economic_support import *
 from app.domain.schemas.applications.economic_support import EconomicSupportCreate, EconomicSupportUpdate
+from app.domain.models import Application_status, User, Application, Status
 
 from app.domain.errors.applications.economic_support import *
 
 from app.core.logging import get_logging
 
-from app.domain.models.user import User
 
 
 log = get_logging(__name__)
 class EconomicSupportPolicy(Base[EconomicSupport, EconomicSupportCreate, EconomicSupportUpdate]):
-    pass
+    # pass
+
+    def create(self, who: User, to: Application) -> tuple[str, str]:
+        #Match student rols (pregraduated and postgraduated)
+        accept_support = to.application_sub_type_id
+
+        if who.rol.scope == 27 : #27 -> pregraduated
+            if accept_support == 13 :
+                raise EconomicSupportErrors(
+                    403, detail=f"No tiene permitido pedir un apoyo económico a Posgrado siendo de pregrado")
+        
+        return 
+    
