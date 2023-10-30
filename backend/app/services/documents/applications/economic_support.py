8f1b1cea863a97d1426fdee4af9a4e76345ef6de
Cristian Gutiérrez <cdavid.gutierrez@udea.edu.co>
2023-10-30 17:18:15 -0500

    cambiando el requirements docx por python-docx y la version de docx a 1.0.1

--- backend/app/services/documents/applications/economic_support.py
+++ backend/app/services/documents/applications/economic_support.py
@@ -25,109 +25,109 @@ log = get_logging(__name__)
 def fill_economic_support_form(user: User, economic_support:EconomicSupportResponse):
     ...
 
-#     economic_support_dict: dict = economic_support.dict()
-#     log.debug("ESTE ES EL DICCIONARIO...")
-#     log.debug(economic_support_dict)
+    economic_support_dict: dict = economic_support.dict()
+    log.debug("ESTE ES EL DICCIONARIO...")
+    log.debug(economic_support_dict)
 
-#     user = UserResponse.from_orm(user).dict(exclude_unset=True)
+    user = UserResponse.from_orm(user).dict(exclude_unset=True)
 
-#     #Rellenar datos de la solicitud.
-#     today = economic_support_dict['created_at'].strftime("%d-%b-%Y")
-#     data_economic_support = {
-#         0:{'today_date':today},
-#         1:{
-#             'type':economic_support_dict['economic_support']['application_data']['application_type'],
-#             'applicant_name':' '.join([user['names'], user['last_names']]).title(),
-#             'identification':user['identification_number'],
-#             'project':economic_support_dict['economic_support']['application_data']['project'],
-#             'objective':economic_support_dict['economic_support']['application_data']['goal'],
-#         },
-#         2:{
-#             'for_vinculado': economic_support_dict['economic_support']['personal_data']['application_for'] if (economic_support_dict['economic_support']['personal_data']['application_for']=='Vinculado UdeA') else (''), #ó Personal externo/Estudiante/Contratista.
-#             'for_external':economic_support_dict['economic_support']['personal_data']['application_for'] if (economic_support_dict['economic_support']['personal_data']['application_for']!='Vinculado UdeA') else (''),
-#             'name':economic_support_dict['economic_support']['personal_data']['name'],
-#             'identification_number':economic_support_dict['economic_support']['personal_data']['identification_number'],
-#             'address':economic_support_dict['economic_support']['personal_data']['address'],
-#             'city':economic_support_dict['economic_support']['personal_data']['city'],
-#             'telephone':economic_support_dict['economic_support']['personal_data']['landline'],
-#             'email':economic_support_dict['economic_support']['personal_data']['email'],
-#             'phone':economic_support_dict['economic_support']['personal_data']['phone'],
-#             'institution':economic_support_dict['economic_support']['personal_data']['institution'],
-#             'academic_unit':economic_support_dict['economic_support']['personal_data']['academic_unit'],
-#         },
-#         3:{
-#             'birth_date':'',
-#             'departure_date':economic_support_dict['economic_support']['tickets']['departure_date'].strftime("%d-%b-%Y"),
-#             'departure_place':economic_support_dict['economic_support']['tickets']['departure_place'],
-#             'birth_place':economic_support_dict['economic_support']['tickets']['place_birth'],
-#             'arrival_date':economic_support_dict['economic_support']['tickets']['arrival_date'].strftime("%d-%b-%Y"),
-#             'arrival_place':economic_support_dict['economic_support']['tickets']['arrival_place']
-#         },
-#         4:{
-#             'payment_name':economic_support_dict['economic_support']['payment']['name'],
-#             'identification':economic_support_dict['economic_support']['payment']['id'],
-#             'bank':economic_support_dict['economic_support']['payment']['bank'],
-#             'value':economic_support_dict['economic_support']['payment']['value'],
-#             'account_number':economic_support_dict['economic_support']['payment']['account_number'],
-#             'account_type':economic_support_dict['economic_support']['payment']['account_type'],#corriente
-#             'start_date':economic_support_dict['economic_support']['payment']['start_date'].strftime("%d-%b-%Y"),
-#             'end_date':economic_support_dict['economic_support']['payment']['end_date'].strftime("%d-%b-%Y")
-#         }
-#     }
+    #Rellenar datos de la solicitud.
+    today = economic_support_dict['created_at'].strftime("%d-%b-%Y")
+    data_economic_support = {
+        0:{'today_date':today},
+        1:{
+            'type':economic_support_dict['economic_support']['application_data']['application_type'],
+            'applicant_name':' '.join([user['names'], user['last_names']]).title(),
+            'identification':user['identification_number'],
+            'project':economic_support_dict['economic_support']['application_data']['project'],
+            'objective':economic_support_dict['economic_support']['application_data']['goal'],
+        },
+        2:{
+            'for_vinculado': economic_support_dict['economic_support']['personal_data']['application_for'] if (economic_support_dict['economic_support']['personal_data']['application_for']=='Vinculado UdeA') else (''), #ó Personal externo/Estudiante/Contratista.
+            'for_external':economic_support_dict['economic_support']['personal_data']['application_for'] if (economic_support_dict['economic_support']['personal_data']['application_for']!='Vinculado UdeA') else (''),
+            'name':economic_support_dict['economic_support']['personal_data']['name'],
+            'identification_number':economic_support_dict['economic_support']['personal_data']['identification_number'],
+            'address':economic_support_dict['economic_support']['personal_data']['address'],
+            'city':economic_support_dict['economic_support']['personal_data']['city'],
+            'telephone':economic_support_dict['economic_support']['personal_data']['landline'],
+            'email':economic_support_dict['economic_support']['personal_data']['email'],
+            'phone':economic_support_dict['economic_support']['personal_data']['phone'],
+            'institution':economic_support_dict['economic_support']['personal_data']['institution'],
+            'academic_unit':economic_support_dict['economic_support']['personal_data']['academic_unit'],
+        },
+        3:{
+            'birth_date':'',
+            'departure_date':economic_support_dict['economic_support']['tickets']['departure_date'].strftime("%d-%b-%Y"),
+            'departure_place':economic_support_dict['economic_support']['tickets']['departure_place'],
+            'birth_place':economic_support_dict['economic_support']['tickets']['place_birth'],
+            'arrival_date':economic_support_dict['economic_support']['tickets']['arrival_date'].strftime("%d-%b-%Y"),
+            'arrival_place':economic_support_dict['economic_support']['tickets']['arrival_place']
+        },
+        4:{
+            'payment_name':economic_support_dict['economic_support']['payment']['name'],
+            'identification':economic_support_dict['economic_support']['payment']['id'],
+            'bank':economic_support_dict['economic_support']['payment']['bank'],
+            'value':economic_support_dict['economic_support']['payment']['value'],
+            'account_number':economic_support_dict['economic_support']['payment']['account_number'],
+            'account_type':economic_support_dict['economic_support']['payment']['account_type'],#corriente
+            'start_date':economic_support_dict['economic_support']['payment']['start_date'].strftime("%d-%b-%Y"),
+            'end_date':economic_support_dict['economic_support']['payment']['end_date'].strftime("%d-%b-%Y")
+        }
+    }
 
-#     path = f'user_{user["id"]}/{uuid1()}'+'formato_apoyo_economico.docx'
+    path = f'user_{user["id"]}/{uuid1()}'+'formato_apoyo_economico.docx'
 
-#     generate_support_format_to_aws.apply_async(args=(data_economic_support, path))
-#     log.debug(path)
-#     return path
+    generate_support_format_to_aws.apply_async(args=(data_economic_support, path))
+    log.debug(path)
+    return path
 
-# @celery_app.task
-# def generate_support_format_to_aws(data_application: dict, path: str):
-#     cells = open(templates_dir + '/cells_support.json')
-#     cells = json.load(cells)
+@celery_app.task
+def generate_support_format_to_aws(data_application: dict, path: str):
+    cells = open(templates_dir + '/cells_support.json')
+    cells = json.load(cells)
 
-#     document = Document(templates_dir + '/en-fo-001.v02-solicitud-avances-viaticos-pasajes-apoyos-economicos.docx')
+    document = Document(templates_dir + '/en-fo-001.v02-solicitud-avances-viaticos-pasajes-apoyos-economicos.docx')
 
-#     for table in range(5):
-#         keys, values = cells[str(table)].keys(), cells[str(table)].values()
-#         for key, cell in zip(keys, values):
-#             document.tables[table].cell(0,cell).text = data_application[str(table)][key]
+    for table in range(5):
+        keys, values = cells[str(table)].keys(), cells[str(table)].values()
+        for key, cell in zip(keys, values):
+            document.tables[table].cell(0,cell).text = data_application[str(table)][key]
 
-#     with NamedTemporaryFile() as tmp:
-#         document.save(tmp.name)
-#         file = BytesIO(tmp.read())
+    with NamedTemporaryFile() as tmp:
+        document.save(tmp.name)
+        file = BytesIO(tmp.read())
 
-#     aws.s3.push_data_to_s3_bucket(settings.aws_bucket_name, file,
-#                                   file_name=path, content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
+    aws.s3.push_data_to_s3_bucket(settings.aws_bucket_name, file,
+                                  file_name=path, content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
 
-# @celery_app.task
+@celery_app.task
 def create_zip_documents(user: User, economic_support:EconomicSupportResponse):
     ...
-#     regex = r"user_[0-9]+/([\w.-]+)"
-#     economic_support_dict: dict = economic_support.dict()
-#     files = economic_support_dict['economic_support']['documents']
-#     zip_name = "{}_{}-{}_{}.zip".format(economic_support_dict['application_sub_type']['name'],
-#                                   economic_support_dict['id'],
-#                                   str.title(economic_support_dict['user']['names']+economic_support_dict['user']['last_names']),
-#                                   economic_support_dict['user']['identification_number']
-#                                   )
-#     file_paths = [file['path'] for file in files]
-#     #Abrir una carpeta temporal.
-#     temp_dir = tempfile.TemporaryDirectory()
-#     # log.debug("Está vacía la carpeta?")
-#     # log.debug(os.listdir(temp_dir.name))
-#     destination = temp_dir.name
-#     binary_files = [os.path.join(destination, re.findall(regex, file)[0]) for file in file_paths]
-#     for file in file_paths:
-#         aws.s3.s3.meta.client.download_file(settings.aws_bucket_name, file, os.path.join(destination, re.findall(regex, file)[0]))
+    regex = r"user_[0-9]+/([\w.-]+)"
+    economic_support_dict: dict = economic_support.dict()
+    files = economic_support_dict['economic_support']['documents']
+    zip_name = "{}_{}-{}_{}.zip".format(economic_support_dict['application_sub_type']['name'],
+                                  economic_support_dict['id'],
+                                  str.title(economic_support_dict['user']['names']+economic_support_dict['user']['last_names']),
+                                  economic_support_dict['user']['identification_number']
+                                  )
+    file_paths = [file['path'] for file in files]
+    #Abrir una carpeta temporal.
+    temp_dir = tempfile.TemporaryDirectory()
+    # log.debug("Está vacía la carpeta?")
+    # log.debug(os.listdir(temp_dir.name))
+    destination = temp_dir.name
+    binary_files = [os.path.join(destination, re.findall(regex, file)[0]) for file in file_paths]
+    for file in file_paths:
+        aws.s3.s3.meta.client.download_file(settings.aws_bucket_name, file, os.path.join(destination, re.findall(regex, file)[0]))
 
-#     # log.debug("Está llena la carpeta?")
-#     # log.debug(os.listdir(temp_dir.name))
+    # log.debug("Está llena la carpeta?")
+    # log.debug(os.listdir(temp_dir.name))
 
-#     zip_buffer = BytesIO()
-#     with ZipFile(zip_buffer,'w', ZIP_DEFLATED) as zip:
-#         # writing each file one by one
-#         for file in binary_files:
-#             zip.write(file)
+    zip_buffer = BytesIO()
+    with ZipFile(zip_buffer,'w', ZIP_DEFLATED) as zip:
+        # writing each file one by one
+        for file in binary_files:
+            zip.write(file)
 
-#     return zip_buffer
+    return zip_buffer
