fbd56b31a7f59b61fcb16e96eb4d1c46b9306503
Hdavid1123 <david.vergel@udea.edu.co>
2024-03-01 11:07:18 -0500

    feat (full-time-list): Cambiando la ubicación de la tabla desde el archivo principal al componente

--- client/src/app/modules/applications/pages/create/components/report-full-time/components/full-time-list/full-time-list.component.ts
+++ client/src/app/modules/applications/pages/create/components/report-full-time/components/full-time-list/full-time-list.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Application } from '@interfaces/application';
+import { FulltimeResponse } from '@interfaces/applications/full_time/full-time';
+import { ApplicationService } from '@services/application.service';
+import { FullTimeService } from '@services/applications/full_time/full-time.service';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-full-time-list',
@@ -7,9 +13,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FullTimeListComponent implements OnInit {
 
-  constructor() { }
+  public applications: Application [] = [];
+  public full_time: FulltimeResponse [] = [];
+  public from_full_time: boolean = true;
+
+  constructor(
+    private fb: FormBuilder,
+    private applicationSvc: ApplicationService,
+    private fullTimeSvc: FullTimeService
+  ) { }
+
+  public form = this.fb.group({
+    from_full_time: [true],
+    title: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(100)]],
+  })
 
   ngOnInit(): void {
   }
 
+  isInvalidForm(controlName: string) {
+    return (
+      this.form.get(controlName)?.invalid && this.form.get(controlName)?.touched
+    );
+  }
 }
