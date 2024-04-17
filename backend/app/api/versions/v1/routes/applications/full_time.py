9d7269d0aa5a8071d3f4560d5960708e7e425962
Hdavid1123 <harversan1123@gmail.com>
2024-04-17 09:50:55 -0500

    feature (api/routes/full_time.py): Creando el endpoint copy_full_time, crea nuevo aplication con mismo archivo de mongo

--- backend/app/api/versions/v1/routes/applications/full_time.py
+++ backend/app/api/versions/v1/routes/applications/full_time.py
@@ -79,6 +79,61 @@ async def create_full_time(
     )
     return response
 
+@router.post('/copy-full-time/{id}', response_model=FullTimeResponse)
+async def copy_full_time(
+    id: int,
+    current_user: User = Depends(jwt_bearer.get_current_active_user),
+    engine: AIOSession = Depends(mongo_db.get_mongo_db),
+    db: Session = Depends(db.get_db)
+) -> FullTimeResponse:
+    """
+    Endpoint to copy a FullTime application
+
+        params:
+            - id: int, this is the id of the full time to copy
+
+        response:
+            - full_time
+    """
+    try:
+        # Obtener la aplicación FullTime original
+        application: Application = crud.application.get(db, current_user, id=id)
+        original_mongo_id = ObjectId(application.mongo_id)
+        original_full_time = await crud.full_time.get(engine, id=original_mongo_id)
+        
+        if original_full_time is None:
+            raise HTTPException(status_code=404, detail="No se encontró la aplicación FullTime")
+        
+        # Crear una copia del objeto FullTime
+        full_time_copied = original_full_time.copy()
+        # Guardar la copia en la base de datos
+        full_time_copied = await crud.full_time.create(db=engine, obj_in=full_time_copied)
+        
+        application = ApplicationCreate(
+            mongo_id=str(full_time_copied.id),
+            application_sub_type_id=application.application_sub_type_id,
+            start_date=application.start_date,
+            user_id=current_user.id
+        )
+        application = crud.application.create(
+            db=db, who=current_user, obj_in=application,
+            status=6, observation=f'El usuario copió la dedicación exclusiva con id: {id}')
+        
+    except BaseErrors as e:
+        raise HTTPException(e.code, e.detail)
+    except ValueError as e:
+        raise HTTPException(422, e)
+    except Exception as e:
+        raise HTTPException(422, "Algo ocurrió mal")
+    
+    # Formatting the response
+    application = ApplicationResponse.from_orm(application)
+    response = FullTimeResponse(
+        **dict(application),
+        full_time=full_time_copied
+    )
+    return response
+
 
 @router.get("/{id}", response_model=FullTimeResponse)
 async def get_full_time(
