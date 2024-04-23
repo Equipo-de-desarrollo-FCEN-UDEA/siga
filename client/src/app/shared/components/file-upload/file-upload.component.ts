c09c218152e672612951488bd8f7b23d78a534ab
smoke <jhon.jaramilloe@udea.edu.co>
2024-04-23 16:43:45 -0500

    :bug: Se arregla el bug que monta más de 3 archivos en editar, para llevar a cabo esto se analizaron los @Input del componente hijo, para saber que enviaba el padre, funciona bien

--- client/src/app/shared/components/file-upload/file-upload.component.ts
+++ client/src/app/shared/components/file-upload/file-upload.component.ts
@@ -1,12 +1,9 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
-import { ApplicationTypesService } from '@services/application-types.service';
-
-
+import { FormBuilder, FormGroup} from '@angular/forms';
+import { NavigationEnd, Router } from '@angular/router';
 //interfaces
 import { file_path } from '@interfaces/documents';
-import { filter, map } from 'rxjs';
+import { filter} from 'rxjs';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -16,16 +13,11 @@ import Swal from 'sweetalert2';
 })
 export class FileUploadComponent implements OnInit {
   // Files
-  public files: any[] = [];
-  public document_new = [1];
-  public documentsToDelete: string[] = []
+  @Input() files: any[] = [];
+  public document_new= [1];
   @Input() documents: file_path[] | any = [];
+  @Input() documentsToDelete: string[] = []
   @Input() applycationType: number = 0;
-
-  @Output() documentsValues = new EventEmitter<any>();
-  @Output() filesValues: EventEmitter<any[]> = new EventEmitter<any[]>();
-
-  // arrayEmitter: EventEmitter<any[]> = new EventEmitter<any[]>();
 
   public form: FormGroup;
   activatedComponentReference: any;
@@ -34,10 +26,7 @@ export class FileUploadComponent implements OnInit {
   get f() {
     return this.form.controls;
   }
-  
-
   constructor(
-    private applicationTypeSvc: ApplicationTypesService,
     private fb: FormBuilder,
     private router: Router
     ) {
@@ -46,35 +35,40 @@ export class FileUploadComponent implements OnInit {
     });
   }
 
-  // --------------------------------------
-  // -------- ARCHIVOS - ANEXOS -----------
-  // --------------------------------------
-
   // Subir un archivo
   onUpload(event: Event, index: number) {
+    if (this.files.length >= 3 && !this.files[index]) {
+      // Si ya hay 3 archivos y el archivo en el índice especificado no existe,
+      // no añadir el nuevo archivo.
+      return;
+    }
     const ELEMENT = event.target as HTMLInputElement;
     const FILE = ELEMENT.files?.item(0);
     if (FILE) {
-      this.files.splice(index, 1, FILE);
+      if (this.files[index]) {
+        // Si el archivo en el índice especificado existe, reemplazarlo.
+        this.files.splice(index, 1, FILE);
+      } else if (this.files.length < 3) {
+        // Si el archivo en el índice especificado no existe y hay menos de 3 archivos,
+        // añadir el nuevo archivo.
+        this.files.push(FILE);
+      }
     }
   }
-
   // Eliminar achivos
   removeFile(index: number) {
-    if (this.files.length > 0) {
-      this.files.splice(index, 1);};
+    if (this.document_new.length > 1) {
+    this.document_new.splice(index, 1);};
+    this.files.splice(index, 1);
   }
-
   // Verifica el tamaño de los archivos que se van a adjuntar al permiso, max:2MB
   validSize() {
     const SIZE = this.files.map((a) => a.size).reduce((a, b) => a + b, 0);
     return SIZE < 6 * 1024 * 1024;
   }
-
-// Verifica que el o los archivos a adjuntar sea de un tipo valido
+  // Verifica que el o los archivos a adjuntar sea de un tipo valido
   validFileType() {
     const VALID_EXTENSIONS = ['png', 'jpg', 'gif', 'jpeg', 'pdf'];
-
     const docs_subidos: string[] = [];
     for (let eachfile of this.files){
       if (VALID_EXTENSIONS.includes(eachfile.name.split('.')[eachfile.name.split('.').length - 1])) {
@@ -86,15 +80,7 @@ export class FileUploadComponent implements OnInit {
     }
     return false; 
   }
-
-  onChanged(): void {
-    this.documentsValues.emit(this.form.value);
-  }
-
-  EmitFiles(): void {
-    this.filesValues.emit(this.files);
-  }
-
+  
   invalidFile() {
     return this.validSize() && this.validFileType();
   }
@@ -125,5 +111,4 @@ export class FileUploadComponent implements OnInit {
       this.isEditRoute = this.router.url.includes('editar');
     });
   }
-
 }
