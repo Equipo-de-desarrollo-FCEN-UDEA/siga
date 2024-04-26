bd93d5c1ed40e7c5ef7315b5b89a9761df21f6c7
FerneyMontoya6 <63945368+FerneyMontoya6@users.noreply.github.com>
2024-04-26 14:54:59 -0500

    Bugfix/full time status (#322)
    * bugfix init
    
    * :bug: Ya deja hacer submit con la fecha seleccionada de la dedicación exclusiva

--- client/src/app/modules/applications/pages/edit/components/full-time-select-date/full-time-select-date.component.ts
+++ client/src/app/modules/applications/pages/edit/components/full-time-select-date/full-time-select-date.component.ts
@@ -1,22 +1,25 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
-import { file_path } from '@interfaces/documents';
-import { FullTimeService } from '@services/applications/full_time/full-time.service';
+import { FormBuilder, Validators } from '@angular/forms'; // Importa FormBuilder y Validators para trabajar con formularios reactivos
+import { ActivatedRoute, Router } from '@angular/router'; // Importa ActivatedRoute y Router para obtener parámetros de ruta y navegar entre rutas
+import { FullTimeCreate } from '@interfaces/applications/full_time/full-time'; // Importa FullTimeCreate desde el módulo de aplicaciones a tiempo completo
+import { file_path } from '@interfaces/documents'; // Importa file_path desde el módulo de documentos
+import { FullTimeService } from '@services/applications/full_time/full-time.service'; // Importa FullTimeService para realizar operaciones relacionadas con eventos a tiempo completo
+import Swal from 'sweetalert2'; // Importa Swal para mostrar mensajes emergentes de forma atractiva
 
 /**
  * Angular component for selecting a start date for full time application.
  */
 @Component({
-  selector: 'app-full-time-select-date',
-  templateUrl: './full-time-select-date.component.html',
-  styleUrls: ['./full-time-select-date.component.scss'],
+  selector: 'app-full-time-select-date', // Selector del componente
+  templateUrl: './full-time-select-date.component.html', // Plantilla HTML del componente
+  styleUrls: ['./full-time-select-date.component.scss'], // Estilos del componente
 })
 export class FullTimeSelectDateComponent {
   /** Identifier of the full-time event. */
-  public id!: number;
+  public id!: number; // Identificador del evento a tiempo completo
+
   /** List of file paths related to the event. */
-  public documents: file_path[] = [];
+  public documents: file_path[] = []; // Lista de rutas de archivos relacionados con el evento
 
   /**
    * Constructor of the component.
@@ -25,20 +28,21 @@ export class FullTimeSelectDateComponent {
    * @param fullTimeService Instance of FullTimeService for fetching event data.
    */
   constructor(
-    private fb: FormBuilder,
-    private route: ActivatedRoute,
-    private fullTimeService: FullTimeService
+    private fb: FormBuilder, // Inyecta el FormBuilder para construir formularios
+    private route: ActivatedRoute, // Inyecta ActivatedRoute para obtener parámetros de ruta
+    private fullTimeService: FullTimeService, // Inyecta FullTimeService para obtener datos del evento
+    private router: Router // Inyecta Router para la navegación entre rutas
   ) {
     // Gets the event ID from the route and loads event data when the component initializes.
+    // Obtiene el ID del evento de la ruta y carga los datos del evento cuando el componente se inicializa.
     this.route.parent?.params.subscribe((params) => {
-      this.id = params['id'];
+      this.id = params['id']; // Obtiene el ID del evento de los parámetros de la ruta
       this.fullTimeService.getFullTime(this.id).subscribe((data) => {
-        console.log(data.full_time);
-
+        // Obtiene datos del evento a través del servicio FullTimeService
         this.form.patchValue({
-          start_date: data.start_date,
-          title: data.full_time.title,
-          documents: data.full_time.documents,
+          start_date: data.start_date, // Establece la fecha de inicio en el formulario
+          title: data.full_time.title, // Establece el título del evento en el formulario
+          documents: data.full_time.documents, // Establece los documentos relacionados con el evento en el formulario
         });
       });
     });
@@ -46,9 +50,9 @@ export class FullTimeSelectDateComponent {
 
   /** Form for selecting the start date and managing documents. */
   public form = this.fb.group({
-    start_date: [new Date(), [Validators.required]],
-    title: ['', [Validators.required]],
-    documents: [this.documents],
+    start_date: [new Date(), [Validators.required]], // Campo de fecha de inicio en el formulario
+    title: ['', [Validators.required]], // Campo de título en el formulario
+    documents: [this.documents], // Campo de documentos en el formulario
   });
 
   /**
@@ -57,11 +61,34 @@ export class FullTimeSelectDateComponent {
    */
   setDates(event: any) {
     this.form.patchValue({
-      start_date: event.start_date,
+      start_date: event, // Método para establecer la fecha de inicio en el formulario
     });
   }
 
+  /**
+   * Method to submit the form data.
+   */
   submit() {
-    console.log('hola mundo');
+    let fullTime = this.fullTimeService.putFullTime(
+      this.form.value as FullTimeCreate, // Envía los datos del formulario al servicio FullTimeService
+      this.id // Envía el ID del evento al servicio FullTimeService
+    );
+    fullTime.subscribe({
+      next: (res) => {
+        // Suscripción al resultado de la operación
+        Swal.fire({
+          // Muestra un mensaje emergente utilizando Swal
+          allowOutsideClick: false,
+          title: 'Dedicación exclusiva en creación',
+          text: 'La fecha se actualizó con éxito',
+          confirmButtonText: 'Continuar',
+          confirmButtonColor: '#3AB795',
+        }).then((result) => {
+          if (result.isConfirmed) {
+            this.router.navigate(['/solicitudes/lista']); // Navega a la lista de solicitudes después de la confirmación
+          }
+        });
+      },
+    });
   }
 }
