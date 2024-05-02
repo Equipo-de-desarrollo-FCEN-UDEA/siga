c8387fda394319250c16f0933b41213a876259fd
smoke <jhon.jaramilloe@udea.edu.co>
2024-05-02 14:02:18 -0500

    :bug: Formato xmls se puede ver en todo los estados, solicitado por la decana

--- client/src/app/modules/applications/pages/view/components/vacation/vacation.component.ts
+++ client/src/app/modules/applications/pages/view/components/vacation/vacation.component.ts
@@ -50,9 +50,6 @@ export class VacationComponent implements OnInit {
       this.application = application;
       let status_app = this.application.application_status[this.application.application_status.length-1].status.name;
       this.vacation.documents = this.vacation.documents!;
-      if (status_app != 'SOLICITADA'){
-        this.vacation.documents.pop();
-      }
       this.current_status = lastElement(
         application.application_status
       ).status.name;
