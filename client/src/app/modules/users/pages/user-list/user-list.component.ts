82728b54b0813d7867b6c5451b2ee37e13e75446
jeronimoloaiza <jeronimo.loaiza@udea.edu.co>
2024-06-07 11:16:50 -0500

    :clown_face: Primer problema del pagination aparentemente resuelto. Ya pasa las páginas en user, sin embargo el pagination no pasa de 10 páginas incluso cuando hay más elementos por mostrar.

--- client/src/app/modules/users/pages/user-list/user-list.component.ts
+++ client/src/app/modules/users/pages/user-list/user-list.component.ts
@@ -22,7 +22,7 @@ export class UserListComponent implements OnInit {
 
   public page = 1;
 
-  public limit = 10;
+  public limit = 100;
 
   private skip = (this.page - 1) * this.limit;
 
@@ -47,11 +47,6 @@ export class UserListComponent implements OnInit {
     this.userSvc.getUsers().subscribe((response) => {
       this.data = response;
       this.totalItems = response.length;
-
-      console.log(this.data);
-      console.log(this.totalItems);
-      
-      
     });
   }
 
